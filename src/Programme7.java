30c1ff2941c225c861b6fdeb150911d9a8a9ce4d
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-26 01:04:16 +0530

    SimplePatternProgramme3ForDiagonalElement2

---
+++ src/Programme7.java
@@ -0,0 +1,11 @@
+public class Programme7 {
+    public static void main(String[] args) {
+        for(int i=5;i>0;i--){
+            for(int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
