7ce46250336a8de0b8a170c17de938616b9d2bd0
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-26 01:22:00 +0530

    SimplePatternProgramme4ForSpaceTerms

--- src/Programme7.java
+++ src/Programme7.java
@@ -1,10 +1,12 @@
 public class Programme7 {
     public static void main(String[] args) {
-        for(int i=5;i>0;i--){
+        for(int i=0;i<5;i++){
             for(int j=5;j>i;j--){
                 System.out.print(" ");
             }
-            System.out.print("*");
+            for(int k=0;k<i;k++){
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
