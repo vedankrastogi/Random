950bb930098423e2cc7d187e677c2b38d6e62769
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-27 16:14:44 +0530

    SimplePatternProgramme4ForSpaceTerms2

---
+++ src/Programme8.java
@@ -0,0 +1,13 @@
+public class Programme8 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
