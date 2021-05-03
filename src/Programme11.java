4d65d7bf17ebc863225d18fa12ea50529de1c624
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-05-03 23:56:00 +0530

    SimplePatternProgramme5ToPrint0StarIn1Line1In2AndSoOn

---
+++ src/Programme11.java
@@ -0,0 +1,16 @@
+ public class Programme11 {
+        public static void main(String[] args) {
+            for(int i=0;i<5;i++){
+                for(int j=i;j>0;j--){
+                    System.out.print("*");
+                }
+                for(int k=i;k<5;k++){
+                    System.out.print(i);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+
