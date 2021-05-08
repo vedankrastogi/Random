6312e288bcdc4b4267d4e7d98247b5d8e76a8358
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-05-08 23:25:06 +0530

    SimplePatternProgramme5ToPrint0StarInReverseDecreasingOrder

---
+++ src/Programme13.java
@@ -0,0 +1,16 @@
+public class Programme13 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=1;j>=j+1;j++){
+                System.out.print(" ");
+            }
+            for(int k=i;k<5;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
