376061bef3734a0dcc478677df5de603ebb7f97c
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-05-09 22:12:00 +0530

    SimplePatternProgramme14ToPrint4StarsInfiniteTimes

---
+++ src/Programme14.java
@@ -0,0 +1,17 @@
+public class Programme14 {
+        public static void main(String[] args) {
+            for(int i=0;i<i+1;i++){
+                for(int j=1;j>=j+1;j++){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<5;k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
+
+
